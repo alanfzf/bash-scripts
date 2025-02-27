fdc36af934d02877334f2f07300b1a00cc195d40
alanfzf <alandavidg13@gmail.com>
2025-02-27 14:27:45 -0600

    feat(k3s): adjust deploy script

--- deploy-k3s.sh
+++ deploy-k3s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TODO: ask for the domain name
+read -p "Please input the domain of your cluster: " CLUSTER_DOMAIN
 
 # install k3s without traefik
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik" sh
@@ -9,17 +9,14 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik" sh
 sudo mkdir -p /etc/rancher/k3s && sudo tee /etc/rancher/k3s/config.yaml > /dev/null <<EOF
 write-kubeconfig-mode: "0644"
 tls-san:
-  - "gepnir.ovh"
+  - "${CLUSTER_DOMAIN}"
 cluster-init: true
 EOF
 
-# copy kube config: https://devops.stackexchange.com/a/16044
+# NOTE: how to use the local storage provider: https://docs.k3s.io/storage#setting-up-the-local-storage-provider
+
+# COPY kube config: https://devops.stackexchange.com/a/16044
 export KUBECONFIG="$HOME/.kube/config"
 mkdir "$HOME/.kube" 2> /dev/null
 sudo k3s kubectl config view --raw > "$KUBECONFIG"
 chmod 600 "$KUBECONFIG"
-
-# fix storage path provider: https://github.com/k3s-io/k3s/issues/85#issuecomment-492475034
-sudo mkdir /opt/local-path-provisioner
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
